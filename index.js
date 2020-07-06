11b7caf763a4924d97ce193b9658a6020086b5d9
Ihnat Klimchuk <ihnat.klimchuk@gmail.com>
2020-07-06 06:16:25 -0400

    Add localhost implementation

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 'use strict';
 
+const axios = require('axios');
+const apiUrl = 'http://localhost:5000/api/is';
+
 var is = function is(x) {
     return new Proxy({}, {
         get(target, property) {
-            return function () {
-                //replace with http call
-                return new Promise(resolve => resolve(undefined));
+            return async function () {
+                try {
+                    const response = await axios.get(apiUrl, { 
+                        params: {
+                            subject: x,
+                            predicate: property
+                        }
+                    });
+                    return response.data.value;
+                } catch (error) {
+                    if(error.response && error.response.status === 400) {
+                        throw new Error(Object.values(error.response.data).flat().join(' '));
+                    } else {
+                        throw error;
+                    }
+                }
             };
         }
     });
--- test/is.test.js
+++ test/is.test.js
@@ -4,16 +4,19 @@ const assert = require('assert');
 const is = require('../');
 
 describe('Simple main test', () => {
-    it('is("habr.com").awesome() return undefined', async () => {
+    it('is("habr.com").awesome() return null', async () => {
         const result = await is("habr.com").awesome();
         assert.equal(result, undefined);
     });
-    it('is("habr.com").amazing() return undefined', async () => {
+    it('is("habr.com").amazing() return null', async () => {
         const result = await is("habr.com").amazing();
         assert.equal(result, undefined);
     });
-    it('is(13).thirteen() return undefined', async () => {
+    it('is(13).thirteen() return null', async () => {
         const result = await is(13).thirteen();
         assert.equal(result, undefined);
+    });
+    it('is().thirteen() throws', async () => {
+        await assert.rejects(async () => { await is().thirteen() }, Error);
     });
 });
